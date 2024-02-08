8732d7cbc4de2112f398027f0914407f9034dbbf
Ilya Sayfutdinov <zukzok043@gmail.com>
2024-02-08 09:23:28 +0300

    Changing small things commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,64 +2,60 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.room.Song
 import com.example.myapplication.room.SongDB
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
 
 const val URL = "https://holychords.pro/"
+const val TAG_TEXT_SONG = "pre"
+const val TAG_NAME_SONG = "h2"
+const val CLASS_AUTHOR_SONG = "text-white d-block d-sm-inline"
+
+val songIds = listOf(57, 16936, 10211, 1798, 39684, 5898, 3, 2606, 2641, 37, 3297,
+    5967, 938, 43704)
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val songsList: RecyclerView
-        val songsAdapter: SongsAdapter
+        val songsList: RecyclerView = findViewById(R.id.songList)
 
-        songsList = findViewById(R.id.songList)
-
-        val layoutManager = LinearLayoutManager(this)
-        songsList.layoutManager = layoutManager
-
-        songsList.setHasFixedSize(true)
-
-        songsAdapter = SongsAdapter()
+        val songsAdapter: SongsAdapter = SongsAdapter()
         songsList.adapter = songsAdapter
 
-        init()
+        requestSite()
 
     }
 
-    private fun getSong(){
-        val db = SongDB.getDB(this)
+    private fun getSong() = GlobalScope.launch(Dispatchers.IO) {
+        val db = SongDB.getDB(applicationContext)
 
-        var doc : Document
-        val i = arrayOf(57, 16936, 10211, 1798, 39684).toMutableList()
+        songIds.forEach { id ->
+            try {
+                val doc = Jsoup.connect("$URL$id").get()
+                val textSong = doc.getElementsByTag(TAG_TEXT_SONG)[1].text()
+                val textName = doc.getElementsByTag(TAG_NAME_SONG)[0].text()
+                val textAuthor = doc
+                    .getElementsByClass(CLASS_AUTHOR_SONG)[0].text()
 
-        for (item in i) {
-            i.forEach {
-                doc = Jsoup.connect(URL + it).get()
-                val textSong : Elements = doc.getElementsByTag("pre")
-                val textName : Elements = doc.getElementsByTag("h2")
-                val textAuthor : Elements = doc
-                    .getElementsByClass("text-white d-block d-sm-inline")
-                db.getDao().insertItem(Song(null, textName[0].text(),
-                    textAuthor[0].text(), textSong[1].text()))
+                db.getDao().insertItem(Song(null, textName, textAuthor, textSong))
+            } catch (e: Exception) {
+                Log.e("MainActivity", "Ошибка при получении данных песни.", e)
             }
         }
     }
 
-    private fun init(){
-        val secTread : Thread
-        val runnable : Runnable = Runnable {
-            run {
-                getSong()
-            }
+    private fun requestSite() {
+        GlobalScope.launch(Dispatchers.Main) {
+            getSong()
         }
-        secTread = Thread(runnable)
-        secTread.start()
     }
 }
